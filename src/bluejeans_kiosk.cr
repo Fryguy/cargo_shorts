f2d317d5ea16399d321ed0b1f38094e4380f5d09
Jason Frey <fryguy9@gmail.com>
2017-06-09 10:16:53 -0400

    Add initial pages and launcher class

--- src/bluejeans_kiosk.cr
+++ src/bluejeans_kiosk.cr
@@ -1,5 +1,42 @@
 require "./bluejeans_kiosk/*"
+require "kemal"
 
-module BluejeansKiosk
-  # TODO Put your code here
+def to_url_with_params(to path, params)
+  query = params.map { |k, v| "#{k}=#{v}" }.join("&")
+  path += "?#{query}" unless query.empty?
+  path
 end
+
+def launcher
+  BluejeansKiosk::Launcher.instance
+end
+
+get "/" do |env|
+  error = env.params.query["error"]?
+  render "views/index.ecr"
+end
+
+get "/start_meeting" do |env|
+  begin
+    meeting_id = env.params.query["meeting_id"]?
+    passcode = env.params.query["passcode"]?
+
+    launcher.start(meeting_id, passcode)
+
+    env.redirect "/"
+  rescue err
+    env.redirect to_url_with_params("/", {"error" => err.message})
+  end
+end
+
+get "/stop_meeting" do |env|
+  begin
+    launcher.stop
+
+    env.redirect "/"
+  rescue err
+    env.redirect to_url_with_params("/", {"error" => err.message})
+  end
+end
+
+Kemal.run
---
+++ src/bluejeans_kiosk/launcher.cr
@@ -0,0 +1,42 @@
+module BluejeansKiosk
+  class Launcher
+    getter meeting_id : String | Nil
+    getter passcode : String | Nil
+    getter? started
+
+    def self.instance
+      @@instance ||= new
+    end
+
+    def initialize
+      # TODO: Detect an already running application and set started = true
+    end
+
+    def bluejeans_url
+      url = "https://bluejeans.com/#{meeting_id}"
+      url = "#{url}/#{passcode}" unless passcode.to_s.empty?
+      url
+    end
+
+    def start(meeting_id, passcode)
+      raise "cannot start if already started" if started?
+      raise "meeting_id cannot be blank" if meeting_id.nil? || meeting_id.empty?
+      @meeting_id = meeting_id
+      @passcode = passcode
+
+      puts "LAUNCHING #{bluejeans_url}"
+
+      @started = true
+    end
+
+    def stop
+      raise "cannot stop if not already started" unless started?
+
+      puts "STOPPING #{meeting_id}"
+
+      @meeting_id = nil
+      @passcode = nil
+      @started = false
+    end
+  end
+end
