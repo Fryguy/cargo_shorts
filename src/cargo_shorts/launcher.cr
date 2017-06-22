859d51b164330d5869cf924ff1bc5b745c24ee35
Jason Frey <fryguy9@gmail.com>
2017-06-22 01:37:53 -0400

    Add ability to pass URL directly

--- src/cargo_shorts/launcher.cr
+++ src/cargo_shorts/launcher.cr
@@ -1,7 +1,6 @@
 module CargoShorts
   class Launcher
-    getter meeting_id : String | Nil
-    getter passcode : String | Nil
+    getter url : String?
     getter? started
 
     def self.instance
@@ -12,35 +11,49 @@ module CargoShorts
       # TODO: Detect an already running application and set started = true
     end
 
-    def bluejeans_url
-      url = "https://bluejeans.com/#{meeting_id}"
-      url = "#{url}/#{passcode}" unless passcode.to_s.empty?
-      url
-    end
-
-    def start(meeting_id, passcode)
+    # Start the meeting with the specified URL or meeting_id/passcode Tuple
+    def start(meeting_info : String | Tuple(String?, String?))
       raise "cannot start if already started" if started?
-      raise "meeting_id cannot be blank" if meeting_id.nil? || meeting_id.empty?
-      @meeting_id = meeting_id
-      @passcode = passcode
 
-      launch_in_chrome
+      @url = meeting_info.is_a?(String) ? validate_url(meeting_info) : build_url(meeting_info)
+
+      launch_in_chrome(@url.not_nil!)
 
       @started = true
     end
 
+    # Stop the current meeting
     def stop
       raise "cannot stop if not already started" unless started?
 
       stop_chrome
 
-      @meeting_id = nil
-      @passcode = nil
+      @url = nil
       @started = false
     end
 
-    private def launch_in_chrome
-      Process.new("/opt/google/chrome/chrome", args: [bluejeans_url, "--kiosk"], env: {"DISPLAY" => "0"})
+    private def build_url(meeting_info)
+      meeting_id, passcode = meeting_info
+      raise "meeting_id cannot be blank" if meeting_id.nil? || meeting_id.empty?
+
+      String.build do |url|
+        url << "https://bluejeans.com/" << meeting_id
+        url << "/" << passcode unless passcode.nil? || passcode.empty?
+      end
+    end
+
+    private def validate_url(url)
+      parsed = URI.parse(url)
+      unless parsed.host.try &.ends_with?("bluejeans.com") &&
+             parsed.path.try &.match(%r{\A/\w+(?:/[0-9]+)?\Z})
+        raise "invalid URL"
+      end
+
+      url
+    end
+
+    private def launch_in_chrome(url)
+      Process.new("/opt/google/chrome/chrome", args: [url, "--kiosk"], env: {"DISPLAY" => "0"})
     end
 
     private def stop_chrome
