b6bb6c51eb5f4fde80abab964249e1d94669c36e
Jason Frey <fryguy9@gmail.com>
2017-06-21 23:47:13 -0400

    Simplify page interactions by specifying @noframes

--- userscripts/cargo_shorts.js
+++ userscripts/cargo_shorts.js
@@ -6,6 +6,7 @@
 // @author       Jason Frey
 // @match        https://bluejeans.com/*
 // @run-at       document-end
+// @noframes
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -15,44 +16,37 @@
 (function() {
     'use strict';
 
-    function isInitialPage() {
-        return !isSubsequentPage();
-    }
-
-    function isSubsequentPage() {
-        return location.pathname == "/xd-local-storage-iframe/";
-    }
-
     function guestLoginInput() {
-        return window.top.document.querySelector(".guestView.panel > div:nth-child(2) > input");
+        return document.querySelector(".guestView.panel > div:nth-child(2) > input");
     }
 
     function guestLoginButton() {
-        return window.top.document.querySelector(".guestView.panel > button");
+        return document.querySelector(".guestView.panel > button");
     }
 
     function computerAudioButton() {
-        return window.top.document.querySelector(".primaryConnectionsDialog[style*=\"visible\"] .joinComputer");
+        return document.querySelector(".primaryConnectionsDialog[style*=\"visible\"] .joinComputer");
     }
 
     function joinMeetingButton() {
-        return window.top.document.querySelector("#modalContentMain > div.options.dynamicTooltipEl > div.decisionsHolder.connectionSwitching > div > div.enterHolder > button");
+        return document.querySelector("#modalContentMain > div.options.dynamicTooltipEl > div.decisionsHolder.connectionSwitching > div > div.enterHolder > button");
     }
 
-    function waitForEntrypoint() {
-        if (isInitialPage()) {
-            if (guestLoginInput()) {
-                doLogin();
-            } else {
-                setTimeout(waitForEntrypoint, 1000);
-            }
+    function waitForLoginInput() {
+        if (guestLoginInput()) {
+            doLogin();
+            waitForComputerAudioButton();
         } else {
-            if (computerAudioButton()) {
-                doAudioOptions();
-                setTimeout(waitForJoinMeetingButton, 1000);
-            } else {
-                setTimeout(waitForEntrypoint, 1000);
-            }
+            setTimeout(waitForLoginInput, 1000);
+        }
+    }
+
+    function waitForComputerAudioButton() {
+        if (computerAudioButton()) {
+            doAudioOptions();
+            waitForJoinMeetingButton();
+        } else {
+            setTimeout(waitForComputerAudioButton, 1000);
         }
     }
 
@@ -89,7 +83,11 @@
         url: "http://localhost:3000/configuration.json",
         onload: function(response) {
             GM_setValue("configuration", response.responseText);
-            waitForEntrypoint();
+            waitForLoginInput();
+        },
+        onerror: function(response) {
+            GM_setValue("configuration", JSON.stringify({"name": "CargoShorts"}));
+            waitForLoginInput();
         }
     });
 })();
